95a0c1272756c5343cf4743091072a3a2efca714
yeasinbinali <yeasinbinali@gmail.com>
2024-06-02 21:51:38 +0600

    banner added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='flex justify-between items-center gap-10 my-20'>
-            <div>
+        <div className='md:flex justify-between items-center gap-10 mt-16 mb-20'>
+            <div className='w-full'>
                 <h1 className='text-5xl'><span className='font-bold'>The Processes more</span> important from <span className='text-main border-b-4 border-primary font-bold'>SmartHR</span> Here!</h1>
+                <p className='my-8'>SmartHR is a cloud-based Human Resource Management System (HRMS) designed to streamline and automate various HR processes, making it easier for companies to manage employee information, payroll, benefits, and compliance.</p>
                 <div>
-                    <p className='mt-10 mb-5 text-2xl'>Get started for free</p>
+                    <p className='mb-5 text-2xl'>Get started for free</p>
                     <form>
                         <input placeholder='Enter your email address...' className='w-80 border-2 border-[whitesmoke] rounded-full' type="email" name="email" />
                         <input className='btn bg-primary text-secondary px-10 py-2 -ml-10 rounded-full' type="button" value="Get Started" />
                     </form>
                 </div>
             </div>
-            <div>
+            <div className='w-full'>
                 <img src="https://i.ibb.co/svknFCZ/home3.png" alt="" />
             </div>
         </div>
