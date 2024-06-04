98106ee06d5835c5bd36c21b1a09ef0cadda8005
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-04 20:24:11 +0600

    dashboard added

--- src/pages/Dashboard/DashboardContent/DashboardContent.jsx
+++ src/pages/Dashboard/DashboardContent/DashboardContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardContent = () => {
     return (
-        <div className='bg-blue-900 flex-1'>
+        <div className='flex-1'>
             <h1>Dashboard content</h1>
         </div>
     );
