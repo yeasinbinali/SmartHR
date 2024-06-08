01468229782a5c9aeae69b7ce30c6a4c52117322
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-08 20:00:07 +0600

    messages added

--- src/hooks/useMessage.jsx
+++ src/hooks/useMessage.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
 import useAxiosPrivate from './useAxiosPrivate';
+import { useQuery } from '@tanstack/react-query';
 
 const useMessage = () => {
     const axiosSecure = useAxiosPrivate();
+    const { data: messages = [] } = useQuery({
+        queryKey: ['message'],
+        queryFn: async () => {
+            const res = await axiosSecure('/message');
+            return res.data;
+        }
+    })
+    return [messages];
 };
 
 export default useMessage;
--- src/pages/Dashboard/AdminDashboard/Messages/Messages.jsx
+++ src/pages/Dashboard/AdminDashboard/Messages/Messages.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PrivateContainerHeader from '../../../../components/PrivateContainerHeader/PrivateContainerHeader';
+import useMessage from '../../../../hooks/useMessage';
 
 const Messages = () => {
+    const [messages] = useMessage();
+    console.log(messages);
     return (
         <div className='mt-5 ml-5'>
             <PrivateContainerHeader title="All Messages from Employees and HR"></PrivateContainerHeader>
+            <div className='grid grid-cols-3 gap-10'>
+                {messages.map(message => <div className='h-40 overflow-y-scroll border-[1px] border-black px-5 py-2'>
+                    <p>Name: {message.name}</p>
+                    <p className='my-2'>Email: <span className='font-bold'>{message.email}</span></p>
+                    <p>Message: {message.message}</p>
+                </div>)}
+            </div>
         </div>
     );
 };
