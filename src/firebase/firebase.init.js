a2e6b31526ddf88856eb2421b2646684be1c0425
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-09 23:25:44 +0600

    env file added

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFA-FUVlpxvQ0r_Cueo6zxggQe0_3qng0",
-  authDomain: "smart-hr-site.firebaseapp.com",
-  projectId: "smart-hr-site",
-  storageBucket: "smart-hr-site.appspot.com",
-  messagingSenderId: "241210143260",
-  appId: "1:241210143260:web:81588933abdb9f0a8065d9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
